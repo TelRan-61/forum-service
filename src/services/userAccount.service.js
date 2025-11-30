4ec80fdf8cbdc010be43bd254602d91472a40155
edward <edwardlfish@gmail.com>
2025-11-30 20:48:41 +0200

    Implement UserAccountService with basic methods scaffolding

---
+++ src/services/userAccount.service.js
@@ -0,0 +1,25 @@
+class UserAccountService {
+    async register(user) {
+        // TODO: add new user and return user profile
+    }
+
+    async getUser(login) {
+        // TODO: return user profile
+    }
+
+    async removeUser(login) {
+        // TODO: remove user and return user profile
+    }
+
+    async updateUser(login, user) {
+        // TODO: update user profile and return user profile
+    }
+
+    async changeRoles(login, role, isAddRole) {
+        // TODO: add or remove role and return user profile
+    }
+
+    async changePassword(login, newPassword) {
+        // TODO: change password
+    }
+}
