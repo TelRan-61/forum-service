2ee4c3b994eb3f2d0676bf06e541fd9579d1d943
edward <edwardlfish@gmail.com>
2025-12-03 20:19:36 +0200

    Implement UserAccount model, repository, and service methods; enhance error handling for user operations

--- src/services/userAccount.service.js
+++ src/services/userAccount.service.js
@@ -1,22 +1,55 @@
+import userAccountRepository from '../repositories/userAccount.repository.js';
+
 class UserAccountService {
     async register(user) {
-        // TODO: add new user and return user profile
+        try {
+            return await userAccountRepository.addUser(user);
+        } catch (e) {
+            console.log(e);
+            throw new Error('User with this login already exists');
+        }
     }
 
     async getUser(login) {
-        // TODO: return user profile
+        const userAccount =
+            await userAccountRepository.findUser(login);
+        if (!userAccount) {
+            throw new Error(`User with login ${login} not found`);
+        }
+        return userAccount;
     }
 
     async removeUser(login) {
-        // TODO: remove user and return user profile
+        const userAccount =
+            await userAccountRepository.removeUser(login);
+        if (!userAccount) {
+            throw new Error(`User with login ${login} not found`);
+        }
+        return userAccount;
     }
 
     async updateUser(login, user) {
-        // TODO: update user profile and return user profile
+        const userAccount =
+            await userAccountRepository.updateUser(login, user);
+        if (!userAccount) {
+            throw new Error(`User with login ${login} not found`);
+        }
+        return userAccount;
     }
 
     async changeRoles(login, role, isAddRole) {
-        // TODO: add or remove role and return user profile
+        role = role.toUpperCase();
+        let userAccount;
+        if (isAddRole) {
+            userAccount = await userAccountRepository.addRole(login, role);
+        } else {
+            userAccount = await userAccountRepository.removeRole(login, role);
+        }
+        if (!userAccount) {
+            throw new Error(`User with login ${login} not found`);
+        }
+        const {roles, userName = login} = userAccount;
+        return {roles, login: userName};
     }
 
     async changePassword(login, newPassword) {
