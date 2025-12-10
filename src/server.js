b7aac94cde7f26f038d5753daad187e6149e9268
edward <edwardlfish@gmail.com>
2025-12-10 20:50:29 +0200

    Add authorization middleware with route-level role and ownership checks
    - Created `authorization` middleware to handle role- and ownership-based access validation.
    - Added route-specific authorization logic for user and forum post actions.
    - Updated `authentication` middleware to exclude paths starting with `/forum/posts`.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Router} from 'express'
 import mongoose from 'mongoose'
 import config from "./config/config.js"
 import postRoutes from "./routes/post.routes.js"
@@ -6,12 +6,24 @@ import userAccountRoutes from "./routes/userAccount.routes.js";
 import errorHandler from "./middlewares/error.middleware.js";
 import authentication from "./middlewares/authentication.middleware.js";
 import {createAdmin} from "./config/initAdmin.js";
+import authorization from "./middlewares/authorization.middleware.js";
+import {ADMIN, MODERATOR} from "./config/constants.js";
 
-const app = express()
+const app = express();
+const authorizationRouter = Router();
 
 app.use(express.json());
 app.use(authentication);
+// app.use(/^\/account\/user\/\w+\/role\/\w+$/, authorization.hasRole(ADMIN));
+authorizationRouter.all('/account/user/:user/role/:role', authorization.hasRole(ADMIN));
+authorizationRouter.patch(['/account/user/:user', '/forum/post/:id/comment/:user'], authorization.isOwner('user'));
+authorizationRouter.delete('/account/user/:user', authorization.isOwnerOrHasRole('user', ADMIN));
+authorizationRouter.post('/forum/post/:author', authorization.isOwner('author'));
+// authorizationRouter.patch('/forum/post/:id/comment/:author', authorization.isOwner('author'));
+authorizationRouter.patch('/forum/post/:id', authorization.isPostAuthor("id"));
+authorizationRouter.delete('/forum/post/:id', authorization.isPostAuthorOrHasRole("id", MODERATOR));
 
+app.use(authorizationRouter);
 app.use('/forum', postRoutes);
 app.use('/account', userAccountRoutes);
 
